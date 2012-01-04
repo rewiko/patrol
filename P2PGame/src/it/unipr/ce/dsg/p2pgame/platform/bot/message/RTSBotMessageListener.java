3608d036e7168f5f5bd4f57a8978eaa24b094157
josermurgav <josermurgav@gmail.com>
2012-01-04 22:50:41 +0000

    Aggiunto meccanismo di sincronizzazione tra i bot per gli scontri

--- P2PGame/src/it/unipr/ce/dsg/p2pgame/platform/bot/message/RTSBotMessageListener.java
+++ P2PGame/src/it/unipr/ce/dsg/p2pgame/platform/bot/message/RTSBotMessageListener.java
@@ -103,6 +103,12 @@ public class RTSBotMessageListener implements Runnable{
 	            this.planetConqueredAction(receivedMessage, os);
 	        }
 		 
+		 if(receivedMessage.getMessageType().equals("BOTSTARTMATCHREQUEST"))
+		 {
+			 this.BotStartMatchRequestAction(receivedMessage, os);
+			 
+		 }
+		 
 	 }
 	 
 	 private void planetConqueredAction(Message receivedMessage, DataOutputStream os) throws IOException
@@ -119,6 +125,115 @@ public class RTSBotMessageListener implements Runnable{
 			os.write((new AckMessage(this.listenerId, this.listenerAddr, this.port, 0, "")).generateXmlMessageString().getBytes());
 		 
 		 
+	 }
+	 
+	 private void BotStartMatchRequestAction(Message receivedMessage, DataOutputStream os) throws IOException
+	 {
+		 BotStartMatchRequestMessage message=new BotStartMatchRequestMessage(receivedMessage);
+		 System.out.println("###############RTSBotMessageListener-->BotStartMatchRequestAction###########################");
+		 //arriva la richiesta
+		 //se sono già in clash
+		 if(this.mybot.inClash)
+		 {
+			 System.out.println("###############IN MATCH###########################");
+			 long timeStamp=Long.parseLong(message.getTimeStamp());
+			
+				 if(this.mybot.peerId.equals(message.getId())) // se ho iniziato uno scontro con lui
+				 {
+					 if(this.mybot.resId.equals(message.getResId()))
+					 {
+						 System.out.println("###############DECISIONE DI CHI INIZIA LO SCONTRO###########################");
+						 	//invio il messaggio
+						 	BotStartMatchResponseMessage response=new BotStartMatchResponseMessage(this.mybot.getOwnerid(),message.getOtherresId(),message.getResId(),Long.toString(this.mybot.timeStamp),true,true);
+							 
+							os.write(response.generateXmlMessageString().getBytes());
+							
+							
+						 
+					 }
+					 else //if(!(this.mybot.resId.equals(message.getResId()))
+					 {
+						 //decido con quale risorse iniziare uso lo stesso algoritmo che per decidere chi inizia lo scontro
+						 //ma invio l'id dell'altra risorsa
+						 
+							 System.out.println("###############RICHIESTE DI SCONTRO DA DUE RISORSE DIVERSE: DECISIONE###########################");
+							 long diff=Math.abs(this.mybot.timeStamp-timeStamp);
+								
+								//devo comparare i due id
+								int compare=this.mybot.resId.compareTo(message.getResId());
+								
+								//se il mio id e maggiore e diff e pari inizio lo scontro se e' minore ed e' dispari inizio lo scontro
+								//altrimenti lo inizia l'altro
+								if((compare>0 && diff%2==0)||(compare<0 && diff%2!=0))
+								{
+									//inizio io con la risorsa che ho in pancia
+									System.out.println("###############RISORSA"+this.mybot.resId+"###########################");
+									 BotStartMatchResponseMessage response=new BotStartMatchResponseMessage(this.mybot.getOwnerid(),message.getOtherresId(),message.getResId(),message.getTimeStamp(),true,false);
+									 
+									 os.write(response.generateXmlMessageString().getBytes());
+								}
+								else
+								{
+									//inizia lui
+									System.out.println("###############RISORSA"+message.getResId()+"###########################");
+									BotStartMatchResponseMessage response=new BotStartMatchResponseMessage(this.mybot.getOwnerid(),message.getOtherresId(),message.getResId(),message.getTimeStamp(),false,true);
+									 
+									 os.write(response.generateXmlMessageString().getBytes());
+									 
+									 
+									
+								}	
+							 
+						 
+						 
+						 
+						 
+					 }
+					 
+				 }
+				 else //if(!this.mybot.peerId.equals(message.getId()))
+				 {
+					 
+					 System.out.println("###############MATCH CON ALTRO PEER###########################");
+					 //se ho gia iniziato uno scontro con un altro do precedenza allo scontro
+					 //quindi sono in match
+					 BotStartMatchResponseMessage response=new BotStartMatchResponseMessage(this.mybot.peerId,this.mybot.resId,message.getResId(),message.getTimeStamp(),true,false);
+					 
+					 os.write(response.generateXmlMessageString().getBytes());
+					 
+				 }
+				 
+			 
+			
+			
+				 
+			 
+		 }
+		 else
+		 {
+			 System.out.println("###############NON SONO IN MATCH###########################");
+			 this.mybot.inClash=true;
+			 //metto id della risorsa nemica e del peer nemico  e controllo 
+			 this.mybot.resId=message.getResId();
+			 this.mybot.peerId=message.getId();
+			 this.mybot.timeStamp=System.currentTimeMillis();
+			 
+			 //devo inviare la risposta e dire che non sono in clash
+			 BotStartMatchResponseMessage response=new BotStartMatchResponseMessage(this.mybot.getOwnerid(),message.getOtherresId(),message.getResId(),message.getTimeStamp(),false,true);
+			 
+			 os.write(response.generateXmlMessageString().getBytes());
+			 
+			 //meccanismo per aspettare esito scontro...
+			 //sleep per evitare inizio simultaneo di un altro scontro
+			 
+			 try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			this.mybot.inClash=false;
+		 }
 	 }
 
 }
