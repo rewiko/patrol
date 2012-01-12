bf490bdeffbe765bacfdfba56666b5a9214808bc
dan.ste8@gmail.com <dan.ste8@gmail.com@2e213c40-c673-98d9-ac1f-c1994414dd67>
2012-01-12 18:12:28 +0000

    Configuration of cheater and honest bot

--- trunk/P2PGame/src/it/unipr/ce/dsg/p2pgame/platform/bot/RTSMainBot2.java
+++ trunk/P2PGame/src/it/unipr/ce/dsg/p2pgame/platform/bot/RTSMainBot2.java
@@ -4,7 +4,7 @@ public class RTSMainBot2 {
 	
 	public  static void main(String [] arg)
 	{
-		Thread th=new Thread(new RTSGameBot2("profiles/profile1.txt","conf/gameconf.txt",6700,"usr1",9990));
+		Thread th=new Thread(new RTSGameBot2("profiles/profile1_cheater.txt","conf/gameconf_cheater.txt",6700,"usr1",9990));
 		th.start();
 		
 	}
