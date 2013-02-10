7fa5b58791a6187aafea829a494de0d77f258165
giorgio.micconi@studenti.unipr.it <giorgio.micconi@studenti.unipr.it@2e213c40-c673-98d9-ac1f-c1994414dd67>
2013-02-10 22:49:05 +0000

    Some other comments.

--- branches/patrol3d/src/it/unipr/ce/dsg/patrol/gui3d/CoordinatesMapping.java
+++ branches/patrol3d/src/it/unipr/ce/dsg/patrol/gui3d/CoordinatesMapping.java
@@ -7,28 +7,38 @@ package it.unipr.ce.dsg.patrol.gui3d;
 import it.unipr.ce.dsg.patrol.gui.message.content.Point;
 
 /**
- *
+ * Provide mapping between PATROL reference system and jMonkeyEngine reference system.
  * @author Benassi Michael Micconi Giorgio
  */
 public class CoordinatesMapping 
-{
-    public String Patrol="patrol";
-    public String jMonkey="jmonkey";
-    
+{   
+    /**
+     * This constructor does nothing
+     */
     public CoordinatesMapping()
     {
         
     }
     
+    /**
+     * Calculate the jMonkeyEngine coordinates from the PATROL coordinates
+     * @param p PATROL coordinates
+     * @param delta displacement from the two reference system
+     * @return jMonkeyEngine coordinates
+     */
     public Coordinate patrolTojMonkey(Point p,double delta)
     {
-        //return new Coordinate(p.getX()-delta,-(p.getY()-delta));
         return new Coordinate(p.getX()-delta,0.0,p.getY()-delta);
     }
     
+    /**
+     * Calculate the PATROL coordinates from the jMonkeyEngine coordinates
+     * @param p jMonkeyEngine coordinates
+     * @param delta displacement from the two reference system
+     * @return PATROL coordinates
+     */
     public Point jMonkeyToPatrol(Coordinate p,double delta)
     {
-        //return new Point(p.getX()+delta,Math.abs(p.getY()-delta));
         return new Point(p.getX()+delta,p.getZ()+delta);
     }
 }
