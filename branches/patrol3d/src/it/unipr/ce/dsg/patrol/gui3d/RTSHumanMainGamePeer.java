4e9d62a53563cc963ce8221106408c114c0ea517
giorgio.micconi@studenti.unipr.it <giorgio.micconi@studenti.unipr.it@2e213c40-c673-98d9-ac1f-c1994414dd67>
2013-02-10 21:42:44 +0000

    Some other comments.

--- branches/patrol3d/src/it/unipr/ce/dsg/patrol/gui3d/RTSHumanMainGamePeer.java
+++ branches/patrol3d/src/it/unipr/ce/dsg/patrol/gui3d/RTSHumanMainGamePeer.java
@@ -3,21 +3,23 @@ package it.unipr.ce.dsg.patrol.gui3d;
 import it.unipr.ce.dsg.patrol.platform.MainGamePeer;
 
 /**
- *
- * @author giorgio
- * Class that launch MainGamePeer for human player.
- * 
- * It manages only the game node without user interface
+ * Class that launch MainGamePeer for human players. (Only main)
+ * It manages only the game node without user interface.
+ * If there isn't the argument, the default value 9998 will be used for the communication port.
+ * @author Michael Benassi Giorgio Micconi
  */
 public class RTSHumanMainGamePeer 
 {
+    /**
+     * 
+     * @param arg arg arg[0] contains the port used for communication. If there isn't the argument the default value 9998 will be used.
+     */
     public static void main(String [] arg)
-	{
-    	int port = 9998;
+    {
+        int port = 9998;
     	if (arg.length >= 1)
-    		port = Integer.parseInt(arg[0].trim());
-    	
+            port = Integer.parseInt(arg[0].trim());
 	MainGamePeer mainpeer=new MainGamePeer(port);
 	mainpeer.start();
-	}
+    }
 }
